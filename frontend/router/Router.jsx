2833cfdc5fa793c3173ab13105e728586adbc624
KC Lim <kclim0528@gmail.com>
2024-12-01 22:46:37 -0800

    created signup page and route

--- frontend/router/Router.jsx
+++ frontend/router/Router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter  } from "react-router-dom";
 import { LoginPage } from "../src/pages/LoginPage";
+import { SignupPage } from "../src/pages/SignupPage";
 import {App} from '../src/App'
 
 const router = createBrowserRouter([
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
     {
         path:'/login',
         element:<LoginPage/>
+    },
+    {
+        path: '/signup',
+        element:<SignupPage/>
     }
 ])
 
