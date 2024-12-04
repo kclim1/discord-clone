94228284e16851b6035ca7979623f75476339f9a
KC Lim <kclim0528@gmail.com>
2024-12-03 20:30:18 -0800

    added dashboard path to router

--- frontend/router/Router.jsx
+++ frontend/router/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter  } from "react-router-dom";
 import { LoginPage } from "../src/pages/LoginPage";
 import { SignupPage } from "../src/pages/SignupPage";
+import { Dashboard } from "../src/pages/Dashboard";
 import {App} from '../src/App'
 
 const router = createBrowserRouter([
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
     {
         path: '/signup',
         element:<SignupPage/>
+    },
+    {
+        path:'/dashboard',
+        element:<Dashboard/>
     }
 ])
 
