03d20808006e1fca1cf1e364c951382f0a9f96d2
KC Lim <kclim0528@gmail.com>
2024-12-07 06:55:22 -0800

    message schema built

---
+++ backend/models/messageSchema.cjs
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const MessageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Message = new mongoose.model("Message", MessageSchema);
+
+module.exports = Message;
