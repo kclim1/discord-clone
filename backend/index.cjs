721497b9efad9923111e3174b0e0019c3e6cb533
KC Lim <kclim0528@gmail.com>
2024-12-08 03:17:16 -0800

    updated index.js to convert data to json and read forms

--- backend/index.cjs
+++ backend/index.cjs
@@ -29,7 +29,8 @@ mongooseConnect();
 
 //cors moved to corsConfig.js
 app.use(cors);
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 // Passport serialization and deserialization
 serializeUser();
 deserializeUser();
@@ -45,6 +46,14 @@ githubStrategy()
 localStrategy()
 //routes moved to auth routes
 app.use(authRoutes)
+
+
+
+
+
+
+
+
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
