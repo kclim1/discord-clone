e72fb3610fd804b8d20de85e43c1ff5df9448972
KC Lim <kclim0528@gmail.com>
2024-12-10 06:52:37 -0800

    conditionally render profile page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,18 +1,34 @@
+import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "../components/Header";
-import { Footer } from "../components/Footer";
 import { Sidebar } from "../components/Sidebar";
+import { Footer } from "../components/Footer";
 
 export const Dashboard = () => {
+  const location = useLocation();
+
+  const isProfilePage = location.pathname === "/dashboard/profile";
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-
-      <div className="flex flex-grow">
-        <Sidebar className=" h-full bg-gray-800 text-white" />
-        <div className="flex-grow bg-slate-500 ">
-          <h1>Dashboard here</h1>
+      
+      <div className="flex flex-grow ">
+        <Sidebar className="w-[20%] h-full bg-gray-800 text-white" />
+        
+        <div className="chat flex-grow bg-slate-500  ">
+          {isProfilePage ? (
+            <div className="w-full h-full ">
+              <Outlet />
+            </div>
+          ) : (
+            <>
+              <h1>Chat Dashboard</h1>
+              {/* add chat related stuff here */}
+            </>
+          )}
         </div>
       </div>
+      
       <Footer />
     </div>
   );
