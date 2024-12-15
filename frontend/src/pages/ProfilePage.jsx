981aaf697ac05d8021115c8833ffeac680058151
KC Lim <kclim0528@gmail.com>
2024-12-15 07:22:47 -0800

    modified profile page to fit the screen

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,32 +1,42 @@
 import { UpdateProfileForm } from "../components/UpdateProfileForm";
-// import {Camera} from 'lucide-react'
 import { X } from "lucide-react";
 import { PictureUploadForm } from "../components/PictureUploadForm";
-import { useNavigate } from "react-router-dom";
+import { useNavigate ,useParams } from "react-router-dom";
+
 
 export const ProfilePage = () => {
+  const {profileId} = useParams()
   const navigate = useNavigate()
   const closePage = (event)=>{
     event.preventDefault()
-    navigate('/dashboard')
+    navigate(`/dashboard/${profileId}`)
   } 
-  return (
-    <div className=" bg-slate-400  h-full w-full ">
-      <X onClick={closePage} className="absolute top-16 right-8 text-white bg-slate-900 hover:text-red-500 rounded-full p-2 border border-white text-3xl" width={45} height={45}/>
-      <div className="max-w-2xl mx-auto  ">
-        <main className="bg-base-300  rounded-xl pt-6 space-y-8">
-          <div className="text-center">
-            <h1 className="text-2xl font-semibold">Profile</h1>
-            <p className="mt-2">Your Profile Information</p>
-          </div>
-          {/* picture goes here */}
-          <PictureUploadForm/>
-          {/* 2.19.37 timestamp  */}
+  
 
-          <div className="text-center">PICTURE GOES HERE</div>
-          <UpdateProfileForm />
-        </main>
-      </div>
+  return (
+    <div className="bg-[#36393f] h-screen w-full profile-container">
+    <div className=" mx-auto">
+      <main className="bg-base-300 rounded-xl pt-6 space-y-8  w-full ">
+  
+        {/* Flex container for top row */}
+        <div className="flex justify-end px-4 w-full">
+          <X 
+            onClick={closePage} 
+            className="text-white bg-slate-900 hover:text-red-500 rounded-full p-2 border border-white text-3xl cursor-pointer" 
+            width={45} 
+            height={45}
+          />
+        </div>
+  
+        <div className="text-center">
+          <h1 className="text-2xl font-semibold">Profile</h1>
+        </div>
+  
+        {/* Picture Upload and Forms */}
+        <PictureUploadForm />
+        <UpdateProfileForm />
+      </main>
     </div>
+  </div>
   );
 };
