bd34abc7353b1f3c2627de7c5ce9693241d077f7
KC Lim <kclim0528@gmail.com>
2024-12-15 07:21:40 -0800

    fetch profile util function complete

--- frontend/utils/fetchProfile.jsx
+++ frontend/utils/fetchProfile.jsx
@@ -3,18 +3,19 @@ import { useProfileStore } from "../store/useProfileStore";
 
 // Profile ID is passed down as a prop by Dashboard
 export const fetchProfile = async (profileId) => {
-  const { setUser, setLoading } = useProfileStore.getState(); // Access Zustand actions
+  const { setUser,  setLoading } = useProfileStore.getState(); // Access Zustand actions
 
   try {
     setLoading(true); 
-
+    console.log('fetching data...')
     const response = await axios.get(`http://localhost:3000/user/${profileId}`);
     if (response.status === 200) {
-      const { username, email, profilePic } = response.data;
+      const { username, email, profilePic , profileId } = response.data;
       setUser({
         username,
         email,
         profilePic,
+        profileId
       });
     }
   } catch (error) {
