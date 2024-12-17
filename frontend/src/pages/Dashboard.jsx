eab22fcc12f80ee6cace5154b4f72d6546c13fda
KC Lim <kclim0528@gmail.com>
2024-12-17 05:26:59 -0800

    refactored dashboard into components

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,59 +1,61 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useParams } from "react-router-dom";
 import { Header } from "../components/Header";
-import { Sidebar } from "../components/Sidebar";
 import { ServerListSidebar } from "../components/ServerListSidebar";
 import { UserFooter } from "../components/UserFooter";
-import { User } from "lucide-react";
+import { useEffect } from "react";
+import { useProfileStore } from "../../store/useProfileStore";
+import { fetchProfile } from "../../utils/fetchProfile";
+import { Toaster } from "sonner";
+import { InputWithEmoji } from "../components/InputWithEmoji";
 
 export const Dashboard = () => {
+  const { loading } = useProfileStore();
+  const { profileId } = useParams();
   const location = useLocation();
-  const isProfilePage = location.pathname === "/dashboard/profile";
+  const isProfilePage = location.pathname === `/dashboard/${profileId}/profile`;
+  useEffect(() => {
+    if (profileId) {
+      fetchProfile(profileId);
+    }
+  }, [profileId]);
+
   return (
     <div className="header-container flex flex-col h-screen overflow-hidden">
       <Header />
+      <Toaster />
       <div className="dashboard-container flex overflow-hidden">
-        <ServerListSidebar/>
+        <ServerListSidebar />
         <div className="channel-container flex flex-col justify-between bg-[#2f3136]">
-            <div className="text-channels bg-[#2f3136] text-white flex-grow ">
-              #text channels
-            </div>
-            <UserFooter/>
+          <div className="text-channels bg-[#2f3136] text-white flex-grow ">
+            #text channels
+          </div>
+          <UserFooter />
         </div>
         <div className=" bg-[#36393f]  text-white flex flex-col flex-grow justify-between">
-          <div className="chat-container ">
-          {isProfilePage && (
-          <div className="w-full h-full">
-            <Outlet />
+          <div className="chat-container flex flex-grow  mx-8 bg-green-400">
+            {isProfilePage ? (
+              loading ? (
+                <div className="flex items-center justify-center w-full h-full">
+                  {/* Render your loading component */}
+                  <p>Loading...</p>
+                </div>
+              ) : (
+                <div className="w-full h-full">
+                  <Outlet />
+                </div>
+              )
+            ) : (
+              <div>
+                <h1>Chat Dashboard</h1>
+              </div>
+            )}
           </div>
-          )}
-          Chat container
-          </div>
-          
-          <input className="m-8 p-4 rounded-lg h-8 text-[#b9bbbe] bg-[#40444b]" placeholder="Send a message..."/>
+            <InputWithEmoji />
+        </div>
+        <div className="bg-[#2C2F33] w-[10%] text-white truncate">
+          Kiss me through the phone
         </div>
       </div>
     </div>
   );
 };
-
-// return (
-//   <div className="h-screen flex flex-col overflow-hidden">
-//     <Header />
-//     <div className="flex flex-grow bg-green-400">
-//       {/* <ServerListSidebar/> */}
-//       <UserFooter/>
-//       <div className="chat flex-grow bg-slate-500">
-//         {isProfilePage ? (
-//           <div className="w-full h-full">
-//             <Outlet />
-//           </div>
-//         ) : (
-//           <div>
-//             <h1>Chat Dashboard</h1>
-//             {/* add chat related stuff here */}
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//   </div>
-// );
