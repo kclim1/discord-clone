19908e888a56cdcefbc474d2e6d474896bcdfc3c
KC Lim <kclim0528@gmail.com>
2024-12-17 05:24:32 -0800

    updated message schema

--- backend/models/messageSchema.cjs
+++ backend/models/messageSchema.cjs
@@ -3,25 +3,33 @@ const mongoose = require("mongoose");
 const MessageSchema = new mongoose.Schema(
   {
     senderId: {
-      type: mongoose.Schema.Types.ObjectId,
+      type: String, // Storing as string
       ref: "User",
       required: true,
     },
     receiverId: {
-      type: mongoose.Schema.Types.ObjectId,
+      type: String, // Storing as string
       ref: "User",
       required: true,
     },
     text: {
       type: String,
+      trim: true,
+      default: "",
     },
     image: {
       type: String,
+      default: null,
+    },
+    messageType: {
+      type: String,
+      enum: ["text", "image", "gif", "system"], // Explicit message types
+      default: "text",
     },
   },
   { timestamps: true }
 );
 
-const Message = new mongoose.model("Message", MessageSchema);
+const Message = mongoose.model("Message", MessageSchema);
 
 module.exports = Message;
