459f869edde44af506f0b3941e4f930e3af51c3c
KC Lim <kclim0528@gmail.com>
2024-12-17 05:26:39 -0800

    refactored user footer

--- frontend/src/components/UserFooter.jsx
+++ frontend/src/components/UserFooter.jsx
@@ -1,26 +1,26 @@
-import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
 import MicIcon from "@mui/icons-material/Mic";
 import HeadphonesIcon from "@mui/icons-material/Headphones";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import Tooltip from "@mui/material/Tooltip";
-import {  useParams} from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-
+import { useParams, useNavigate } from "react-router-dom";
+import { AddFriendDialog } from "./AddFriendDialog";
 
 export const UserFooter = () => {
-    const {profileId} = useParams()
+  const { profileId } = useParams();
   const navigate = useNavigate();
+
   const handleClick = () => {
-    console.log(profileId)
+    console.log(profileId);
     navigate(`/dashboard/${profileId}/profile`);
   };
+
   return (
-    <div
-      onClick={handleClick}
-      className="flex items-center justify-between p-3 bg-[#292b2f] text-white "
-    >
+    <div className="flex items-center justify-between p-3 bg-[#292b2f] text-white ">
       <Tooltip title="Profile Page">
-        <div className="flex items-center space-x-3 hover:bg-slate-400 pr-2">
+        <div
+          onClick={handleClick}
+          className="flex items-center space-x-3 hover:bg-slate-400 pr-2"
+        >
           <AccountCircleIcon className="w-10 h-10" />
           <div className="leading-tight">
             <p className="text-sm font-semibold">Username</p>
@@ -42,9 +42,7 @@ export const UserFooter = () => {
           </Tooltip>
         </div>
         <div className="p-2 rounded hover:bg-slate-400 cursor-pointer">
-          <Tooltip title="Add Friend">
-            <ManageAccountsIcon className="text-gray-300" />
-          </Tooltip>
+          <AddFriendDialog />
         </div>
       </div>
     </div>
