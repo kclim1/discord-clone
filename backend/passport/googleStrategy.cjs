dfa52724f25e612cdf8259d3cdf6393cac365494
KC Lim <kclim0528@gmail.com>
2024-12-20 07:58:50 -0800

    updated google strategy

--- backend/passport/googleStrategy.cjs
+++ backend/passport/googleStrategy.cjs
@@ -1,32 +1,34 @@
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20')
-const User = require('../models/userSchema.cjs')
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const User = require("../models/userSchema.cjs");
 
- const googleStrategy = ()=>{
+const googleStrategy = () => {
   passport.use(
     new GoogleStrategy(
       {
         clientID: process.env.GOOGLE_CLIENTID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'http://localhost:3000/auth/google/callback',
+        callbackURL: "http://localhost:3000/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, cb) => {
         try {
-          console.log('profile id : ' ,profile.id)
-          let googleUser = await User.findOne({ profileId : profile.id });
+          console.log("profile id : ", profile.id);
+          let googleUser = await User.findOne({ profileId: profile.id });
           if (!googleUser) {
-              googleUser = await User.create({
+            googleUser = await User.create({
               profileId: profile.id,
               username: profile.displayName,
-              email : profile.emails[0].value,
-              familyName:profile.name.familyName,
-              givenName:profile.name.givenName,
-              loginTime: Date.now()
+              email: profile.emails[0].value,
+              familyName: profile.name.familyName,
+              givenName: profile.name.givenName,
+              loginTime: Date.now(),
+              friends: [],
+              isOnline: false,
             });
-  
+
             await googleUser.save();
           }
-          console.log('google auth done')
+          console.log("google auth done");
           cb(null, googleUser);
         } catch (error) {
           cb(error);
@@ -34,7 +36,6 @@ const User = require('../models/userSchema.cjs')
       }
     )
   );
- }
+};
 
-
-  module.exports = googleStrategy
+module.exports = googleStrategy;
