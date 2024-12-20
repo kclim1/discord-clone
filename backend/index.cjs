ca4d11d96dab6cbfe2181b3d7e02c35189cac81d
KC Lim <kclim0528@gmail.com>
2024-12-20 07:34:20 -0800

    implemented socketio

--- backend/index.cjs
+++ backend/index.cjs
@@ -4,11 +4,16 @@ require("dotenv").config();
 const http = require("http");
 const socketIo = require("socket.io");
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 const router = require("express-router");
 const expressValidator = require("express-validator");
 const path = require("path");
-const bcryptjs = require('bcryptjs')
+const bcryptjs = require("bcryptjs");
 const mongooseConnect = require("./models/mongooseConnect.cjs");
 const mongoose = require("mongoose");
 const User = require("./models/userSchema.cjs");
@@ -19,18 +24,17 @@ const serializeUser = require("./passport/serializeUser.cjs");
 const deserializeUser = require("./passport/deserializeUser.cjs");
 const cors = require("./middleware/corsConfig.cjs");
 const googleStrategy = require("./passport/googleStrategy.cjs");
-const localStrategy = require('./passport/localStrategy.cjs')
+const localStrategy = require("./passport/localStrategy.cjs");
 const githubStrategy = require("./passport/githubStrategy.cjs");
-const authRoutes = require('./routes/authRoutes.cjs')
-
+const authRoutes = require("./routes/authRoutes.cjs");
 
 // Connect mongoose
 mongooseConnect();
 
 //cors moved to corsConfig.js
 app.use(cors);
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 // Passport serialization and deserialization
 serializeUser();
 deserializeUser();
@@ -42,21 +46,31 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Set up Google OAuth Strategy with Passport
-googleStrategy()
+googleStrategy();
 //google strat moved to googleStrategy.js
-githubStrategy()
-localStrategy()
+githubStrategy();
+localStrategy();
 //routes moved to auth routes
-app.use(authRoutes)
+app.use(authRoutes);
 
+io.on("connection", (socket) => {
+  console.log(`User has connected: ${socket.id}`);
 
+  socket.on("joinRoom", (room) => {
+    socket.join(room);
+    console.log(`User ${socket.id} joined room: ${room}`);
+  });
 
+  socket.on("sendMessage", ({ room, message }) => {
+    console.log(`Message received in room ${room}: ${message}`);
+    io.to(room).emit("receiveMessage", { sender: socket.id, message });
+  });
 
-
-
-
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
