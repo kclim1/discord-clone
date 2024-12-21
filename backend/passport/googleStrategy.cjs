9cc632f7a89669e696eb862ef5e48d4ff9475ee2
KC Lim <kclim0528@gmail.com>
2024-12-21 07:41:58 -0800

    modifed to store profile pic

--- backend/passport/googleStrategy.cjs
+++ backend/passport/googleStrategy.cjs
@@ -24,6 +24,7 @@ const googleStrategy = () => {
               loginTime: Date.now(),
               friends: [],
               isOnline: false,
+              profilePic: ""
             });
 
             await googleUser.save();
