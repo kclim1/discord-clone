4fa3ed633fb43695f910634334c104863b323ce6
KC Lim <kclim0528@gmail.com>
2024-12-24 07:13:00 -0800

    displays friend list

--- frontend/src/components/FriendListIcon.jsx
+++ frontend/src/components/FriendListIcon.jsx
@@ -1,24 +1,16 @@
 import PersonIcon from "@mui/icons-material/Person";
-// import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 export const FriendListIcon = () => {
-//   const {profileId} = useNavigate()
-  const [click, setClick] = useState(false);
+  const { profileId } = useParams();
 
-  const handleClick = () => {
-    setClick((prev) => !prev);
-  };
   return (
-    <div
-      onClick={handleClick}
-      className={`text-center text-xl flex  space-x-2 p-2 m-4 hover:bg-gray-600 rounded-lg focus:bg-grey-600 ${
-        click ? "bg-gray-600" : ""
-      }
-`}
+    <Link
+      to={`/dashboard/${profileId}/friends`}
+      className="text-center text-xl flex space-x-2 p-2 m-4 hover:bg-gray-600 rounded-lg"
     >
       <PersonIcon />
       <p>Friends</p>
-    </div>
+    </Link>
   );
 };
