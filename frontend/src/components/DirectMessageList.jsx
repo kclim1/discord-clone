f4b8fae4a1afcf317e69ad05b7a053783b421f7d
KC Lim <kclim0528@gmail.com>
2024-12-25 07:31:19 -0800

    drafted direct message list

--- frontend/src/components/DirectMessageList.jsx
+++ frontend/src/components/DirectMessageList.jsx
@@ -1,11 +1,24 @@
-import { FriendListIcon } from './FriendListIcon';
-import { NewDirectMessage } from './NewDirectMessages';
+import { FriendListIcon } from "./FriendListIcon";
+import { NewDirectMessage } from "./NewDirectMessages";
+// import { fetchSession } from '../../utils/fetchSession';
+// import { useEffect } from 'react';
+// import { OnlineIndicator } from './OnlineIndicator';
+// import { OfflineIndicator } from './OfflineIndicator';
+// import { fetchChat } from "../../utils/fetchChat";
+// import { useEffect } from "react";
+import { ChatUI } from "./FriendPageComponents/ChatUI";
 
-export const DirectMessageList = ()=>{
-    return(
-        <div>
-           <FriendListIcon/>
-           <NewDirectMessage/>
-        </div>
-    )
-}
+export const DirectMessageList = () => {
+ 
+
+  return (
+    <div>
+      <FriendListIcon />
+      <NewDirectMessage />
+      <ChatUI/>
+    </div>
+  );
+};
+
+// this is the list that displays who your friends are and who you are chatting with
+// friends list basically
