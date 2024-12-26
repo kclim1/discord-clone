2ddc9308683b3c21fe50d56834a4d0a2a4526df3
KC Lim <kclim0528@gmail.com>
2024-12-26 08:20:15 -0800

    refactored serverlistsidebar to be neater

--- frontend/src/components/ServerListSidebar.jsx
+++ frontend/src/components/ServerListSidebar.jsx
@@ -1,17 +1,12 @@
-import ChatIcon from "@mui/icons-material/Chat";
-import { PurpleTooltip, IconWrapper } from "./PurpleTooltip";
+import { DirectMessagesButton } from "./DirectMessagesButton";
 import { ServerIconTooltip, ServerIconWrapper } from "./AddServerIcon";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import { ServerIcons } from "./ServerIcons";
 
 export const ServerListSidebar = () => {
   return (
-    <div className="w-[25%] bg-slate-950 flex flex-col   items-center h-full py-4  iconContainer">
-      <PurpleTooltip title="Direct Messsages" placement="right">
-        <IconWrapper>
-          <ChatIcon style={{ fontSize: "48px" }} />
-        </IconWrapper>
-      </PurpleTooltip>
+    <div className="min-w-[5%] bg-[#202225] flex flex-col   items-center h-screen py-4  iconContainer">
+      <DirectMessagesButton/>
       <hr className="w-3/4 border-t-2 border-white mt-3 py-2" />
 
       <div className="flex flex-col items-center gap-3 my-3 serverIcons overflow-y-auto h-screen w-full scrollbar-hide "  style={{
@@ -21,7 +16,7 @@ export const ServerListSidebar = () => {
         
         {/* Example Server Icon */}
         <ServerIcons/>
-        <ServerIcons/>
+
         
         
         {/* Add more server icons dynamically here */}
